4d0b18f1ebbc48283f15dbf4473743063599e8f1
Sarath <sarathsaleem@gmail.com>
2015-04-13 02:09:29 +0400

    Path mapped

--- js/graph/render/g8.js
+++ js/graph/render/g8.js
@@ -29,17 +29,23 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
             var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
             d.days = diffDays;
         });
+        
+        function sortByDateAscending(a, b) {
+            return a.dod - b.dod;
+        }
+        data = data.sort(sortByDateAscending);
+        
+        console.log(data);
 
-        console.log(data)
-
-
-        var chartW = canvasWidth - paddingleft,
-            chartH = canvasHeight - paddingBottom;
+        var chartW = canvasWidth - 100,
+            chartH = 700,
+            padding = 50;
+        
         var x = d3.time.scale()
-            .range([0, chartW]);
-
+            .range([padding, chartW]);
+        
         var y = d3.scale.linear()
-            .range([chartH, 0]);
+            .range([chartH, padding]);
 
         var xAxis = d3.svg.axis()
             .scale(x)
@@ -48,9 +54,7 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
         var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left");
-
-
-
+        
         x.domain(d3.extent(data, function (d) {
             return d.dod;
         }));
@@ -61,15 +65,15 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
 
         Chart.append("g")
             .attr("class", "x axis")
-            .attr("transform", "translate(0," + chartH + ")")
+            .attr("transform", "translate(0," + (chartH+10) + ")")
             .call(xAxis);
         Chart.append("g")
             .attr("class", "y axis")
-            .attr("transform", "translate(" + 40 + ",0)")
+            .attr("transform", "translate("+(padding- 20)+", 0)")
             .call(yAxis);
 
-        var cy = d3.scale.linear().domain([0, 365]).range([chartH-40, 0]),
-            cx = d3.time.scale().domain([new Date('01/01/1950'), new Date('01/01/2015')]).range([0, chartW]);
+        var cy = d3.scale.linear().domain([0, 365]).range(y.range()),
+            cx = d3.time.scale().domain([data[0].dod, data[data.length-1].dod]).range(x.range());
 
      var colors = d3.scale.category20();
 
@@ -77,36 +81,63 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
         var stars = Chart.selectAll(".line")
             .data(data)
             .enter()
-            .append('rect')
-            .attr('x',  function (d) {
+            .append('g').attr('class', 'group');
+        
+        var artists = stars.append('circle').attr('cx', 0)
+            .attr('cy',  function (d) {
+               return cy(d.days);
+            })
+            .attr("r", 20)
+            .style("fill", function(d, i) { return colors(i); })
+            .transition()
+            .duration(1000)
+            .attr('cx',  function (d) {
+                return cx(d.dod);
+            });
+        
+        
+        stars.append('text').text(function (d) {
+            return d.name;
+        }).attr('x',  function (d) {
                 return cx(d.dod);
             })
             .attr('y',  function (d) {
-               return cy(d.days);
-            })
-            .attr('width', 40)
-            .attr('height', 40)
-            .style("fill", function(d, i) { return colors(i); });
-        /*
+               return cy(d.days) - 30;
+            }).attr('class', 'label-name');
+       
        var line = d3.svg.line().interpolate('basis');
 
-        var drawPath = function (start) {
-                return function (l) {
-                    var points = [
-                        [100, 100], [450, 100], [450, 800], [800, 800]
-                    ];
-                    return line(points);
-                };
+        
+        
+        var drawPath = function (data, i) {
+                
+             var ele = d3.select(artists[0][i]),
+                ele2 = d3.select(artists[0][i+1] || artists[0][0]);
+            console.log(ele2.data())
+       
+            var x1 =  cx(data.dod),
+                y1 =  cy(data.days),
+                x2 =  cx(ele2.data()[0].dod),
+                y2 = cy(ele2.data()[0].days);
+            
+                var d = Math.abs(x1-x2)/2;
+                
+                var points = [
+                        [x1, y1], [d, y1], [d, x2], [x2, y2]
+                ];
+                return line(points);
+                
             };
 
+        
         var links = Chart.selectAll('path.link')
-                .data([1])
+                .data(data)
                 .enter().append('svg:path')
                 .attr('class', 'link')
                 .style('stroke-width', function (l) {
-                    return 50;
+                    return 20;
                 })
-                .attr('d', drawPath())
+                .attr('d',   drawPath )
                 .on('mouseover', function () {
                     d3.select(this).attr('class', 'link on')
                 })
@@ -115,9 +146,9 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
                 })
                 .transition()
                 .duration(100)
-                .attr('d', drawPath());
-
-    */
+                .attr('d',  drawPath);
+      
+   
 
     }
     return render;
