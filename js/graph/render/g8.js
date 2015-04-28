01f264bc332bb23851197380b3519dfb0e586994
Sarath <sarathsaleem@gmail.com>
2015-04-29 02:45:43 +0400

    Exp labelForce

--- js/graph/render/g8.js
+++ js/graph/render/g8.js
@@ -4,7 +4,66 @@
 define(['d3', 'utils/utils'], function (ignore, _util) {
 
     "use strict";
+    
+    (function() {
+      d3.force_labels = function force_labels() {    
+        var labels = d3.layout.force();
 
+        // Update the position of the anchor based on the center of bounding box
+        function updateAnchor() {
+          if (!labels.selection) return;
+          labels.selection.each(function(d) {
+            var bbox = this.getBBox(),
+                x = bbox.x + bbox.width / 2,
+                y = bbox.y + bbox.height / 2;
+
+            d.anchorPos.x = x;
+            d.anchorPos.y = y;
+
+            // If a label position does not exist, set it to be the anchor position 
+            if (d.labelPos.x == null) {
+              d.labelPos.x = x;
+              d.labelPos.y = y;
+            }
+          });
+        }
+
+        //The anchor position should be updated on each tick
+        labels.on("tick.labels", updateAnchor);
+
+        // This updates all nodes/links - retaining any previous labelPos on updated nodes
+        labels.update = function(selection) {
+          labels.selection = selection;
+          var nodes = [], links = [];
+          selection[0].forEach(function(d) {    
+              console.log(d.__data__)
+            if(d && d.__data__) {
+              var data = d.__data__;
+
+              if (!d.labelPos) d.labelPos = {fixed: false};
+              if (!d.anchorPos) d.anchorPos = {fixed: true};
+
+              // Place position objects in __data__ to make them available through 
+              // d.labelPos/d.anchorPos for different elements
+              data.labelPos = d.labelPos;
+              data.anchorPos = d.anchorPos;
+
+              links.push({target: d.anchorPos, source: d.labelPos});
+              nodes.push(d.anchorPos);
+              nodes.push(d.labelPos);
+            }
+          });
+          labels
+              .stop()
+              .nodes(nodes)
+              .links(links);
+          updateAnchor();
+          labels.start();
+        };
+        return labels;
+      };
+    })();
+    //http://bl.ocks.org/ZJONSSON/1691430
 
     function render(data, canvas) {
 
@@ -86,6 +145,36 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
             }
         });
 
+        var x = d3.time.scale()
+            .range([paddingLeft, chartW]);
+
+        var y = d3.scale.linear()
+            .range([chartH + paddingTop - margin, paddingTop]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        x.domain(d3.extent(artistdata, function (d) {
+            return d.dod;
+        }));
+        y.domain(d3.extent(artistdata, function (d) {
+            return d.dod.getMonth() + 1;
+        }));
+        
+
+        var cy = d3.scale.linear().domain([0, 365]).range(y.range()),
+            cx = d3.time.scale().domain([artistdata[0].dod, artistdata[artistdata.length - 1].dod]).range(x.range());
+
+        artistdata.forEach(function (d) {
+            d.x =  cx(d.dod);
+            d.y =  cy(d.days);
+        });
+
         var stars = Chart.selectAll(".group")
             .data(artistdata)
             .enter()
@@ -108,36 +197,95 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
         stars.attr('class', 'group').attr("transform", function (d, i) {
             return "translate(-200, " + i * 15 + ")";
         });
+        
+        var mode = 'timeline';
+        
+        var tooltip = d3.select(canvas)
+                .append("div").attr("class", "tooltip")
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("visibility", "hidden");
 
+
+        stars.on("mouseover", function (d) {
+            
+                var text = '';
+              
+                if (mode === 'cause') {
+                    text = d.cause;
+                } else {
+                    text = d.name +' <br/>'+ d.dod.getDate() +'/'+ d.dod.getMonth() +'/'+ d.dod.getFullYear();                    
+                }
+            
+                return tooltip.style("visibility", "visible").html(text);
+            })
+            .on("mousemove", function () {
+                var offset = $(canvas).offset();
+                return tooltip.style("top", (event.pageY - offset.top + 30) + "px").style("left", (event.pageX - offset.left) + "px");
+            })
+            .on("mouseout", function () {
+                return tooltip.style("visibility", "hidden");
+            });
+        
+        
+        
+        
+            var labels = Chart.selectAll(".labels").data(artistdata, function (d, i) {
+                return i;
+            });
+            labels.exit().attr("class", "exit").transition().delay(0).duration(500).style("opacity", 0).remove();
+
+            // Draw the labelbox, caption and the link
+            var newLabels = labels.enter().append("g").attr("class", "labels")
+
+            var newLabelBox = newLabels.append("g").attr("class", "labelbox");
+            newLabelBox.append("circle").attr("r", 10)
+            newLabelBox.append("text").attr("class", "labeltext").attr("y", 6);
+            newLabels.append("line").attr("class", "link");
+
+            var labelBox = Chart.selectAll(".labels").selectAll(".labelbox");
+            var links = Chart.selectAll(".link");
+            labelBox.selectAll("text").text(function (d) {
+                return d.name;
+            });
+        
+       function redrawLabels() {
+            labelBox
+                .attr("transform",function(d) { return "translate("+d.labelPos.x+","+d.labelPos.y+")";});
+
+            links
+                .attr("x1",function(d) { return d.anchorPos.x;})
+                .attr("y1",function(d) { return d.anchorPos.y;})
+                .attr("x2",function(d) { return d.labelPos.x;})
+                .attr("y2",function(d) { return d.labelPos.y;});
+       }    
+
+
+         // Initialize the label-forces
+        var labelForce = d3.force_labels()
+            .linkDistance(0.0)
+            .gravity(0)
+            .nodes([]).links([])
+            .charge(60)
+            .on("tick", redrawLabels);
+        
+        labelForce.charge(60).start();   
+        
+        
+        
+        
+        
+        
+        
 
         /**************** Timline **********************/
 
         function timelineMode() {
+            
+            mode = 'timeline';
 
             d3.selectAll('.axis').remove();
             d3.selectAll('.bg-cause').remove();
-
-            var x = d3.time.scale()
-                .range([paddingLeft, chartW]);
-
-            var y = d3.scale.linear()
-                .range([chartH + paddingTop - margin, paddingTop]);
-
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom");
-
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient("left");
-
-            x.domain(d3.extent(artistdata, function (d) {
-                return d.dod;
-            }));
-            y.domain(d3.extent(artistdata, function (d) {
-                return d.dod.getMonth() + 1;
-            }));
-
 
             Chart.append("g")
                 .attr("class", "x axis")
@@ -148,115 +296,56 @@ define(['d3', 'utils/utils'], function (ignore, _util) {
                 .attr("transform", "translate(" + (paddingLeft - margin) + ", 0)")
                 .call(yAxis);
 
-            var cy = d3.scale.linear().domain([0, 365]).range(y.range()),
-                cx = d3.time.scale().domain([artistdata[0].dod, artistdata[artistdata.length - 1].dod]).range(x.range());
-
-            stars.transition().duration(1000).attr("transform", function (d) {
-                d.node = {};
-                d.node.x = cx(d.dod);
-                d.node.y = cy(d.days);
-                d.x = cx(d.dod);
-                d.y = cy(d.days);  
-                return "translate(" + cx(d.dod) + ", " + cy(d.days) + ")";
+            stars.transition().duration(1000).attr("transform", function (d) {                
+                return "translate(" + d.x + ", " + d.y + ")";                
             });
             
-            setTimeout(function(){
-                correctTextRendering();
-            }, 2000)
-            
-           
-            
-           
+            stars.call(labelForce.update);
 
 
-        }
-        
-        function correctTextRendering() {
-            var updateNode = function () {
-                this.attr("transform", function (d) {
-                    return "translate(" + d.x + "," + d.y + ")";
-                });
-
-            }
-            stars.each(function (d, i) {
-                if (i % 2 == 0) {
-                    d.x = d.node.x;
-                    d.y = d.node.y;
-                } else {
-                    var b = this.childNodes[1].getBBox();
-
-                    var diffX = d.x - d.node.x;
-                    var diffY = d.y - d.node.y;
-
-                    var dist = Math.sqrt(diffX * diffX + diffY * diffY);
-
-                    var shiftX = b.width * (diffX - dist) / (dist * 2);
-                    shiftX = Math.max(-b.width, Math.min(0, shiftX));
-                    var shiftY = 5;
-                    this.childNodes[1].setAttribute("transform", "translate(" + (shiftX || 0) + "," + shiftY + ")");
-                }
-            });
-            stars.call(updateNode);
         }
 
         /**************** Timline **********************/
 
         function cauase() {
+            
+            mode = 'cause';
 
             d3.selectAll('.axis').remove();
-            d3.selectAll('.bg-cause').remove();
-            d3.selectAll('.tooltip').remove();
-
-            var tooltip = d3.select(canvas)
-                .append("div").attr("class", "tooltip")
-                .style("position", "absolute")
-                .style("z-index", "10")
-                .style("visibility", "hidden");
-
-
-
+            d3.selectAll('.bg-cause').remove();           
+           
             var topMargain = 200,
                 top = 0,
                 left = 50,
-                lefts = [30, 250, 475, 695, 915, 1135],//corrected lefts
+                lefts = [30, 250, 475, 695, 915, 1135], //corrected lefts
                 pos = {},
                 count = {};
-            
+
             stars.transition().duration(1000).attr("transform", function (d) {
                 if (d.type) {
                     left = ((d.type - 1) * 220) + 30;
-                    
-                    pos[d.type] ?  pos[d.type] += 50 : pos[d.type] = topMargain;
-                   
-                    count[d.type] ?  count[d.type] +=1 : count[d.type] = 1;
-                    
+
+                    pos[d.type] ? pos[d.type] += 50 : pos[d.type] = topMargain;
+
+                    count[d.type] ? count[d.type] += 1 : count[d.type] = 1;
+
                     top = pos[d.type];
                 }
                 return "translate(" + lefts[d.type - 1 || 0] + ", " + top + ")";
             });
-
-            stars.on("mouseover", function (d) {
-                return tooltip.style("visibility", "visible").text(d.cause);
-            })
-                .on("mousemove", function () {                  
-                    var offset = $(canvas).offset();
-                    return tooltip.style("top", (event.pageY - offset.top + 30) + "px").style("left", (event.pageX - offset.left) + "px");
-                })
-                .on("mouseout", function () {
-                    return tooltip.style("visibility", "hidden");
-                });
 
             var rects = d3.select(canvas).selectAll(".bg-cause")
                 .data(data.types)
                 .enter()
                 .append('div').attr("class", "bg-cause")
                 .html(function (d) {
-                    var percentage =  (count[d.index]/artistdata.length)*100;
-                    return d.name + '<br/><span>'+percentage.toFixed(2) +'%</span>';
+                    var percentage = (count[d.index] / artistdata.length) * 100;
+                    return d.name + '<br/><span>' + percentage.toFixed(2) + '%</span>';
                 });
 
-
         }
+        
+        timelineMode();
 
         /*
         
