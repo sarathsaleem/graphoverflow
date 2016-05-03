d7b38a7459d8eb4d33ceb7fc7d031aeebc5d0359
sarathsaleem <sarathsaleem@gmail.com>
2016-05-03 22:37:37 +0400

    Clean build -- grunt modifications

--- js/graph/render/g9/lightUp.js
+++ js/graph/render/g9/lightUp.js
@@ -139,7 +139,7 @@ define(['libs/three', 'libs/dat.gui'], function (ignore) {
         this.lights.push(light);
 
 
-        $(container).append(gui.domElement);
+        //$(container).append(gui.domElement);
 
         gui.close();
 
